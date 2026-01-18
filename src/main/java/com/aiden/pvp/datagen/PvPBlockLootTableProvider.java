cda63761ed61bb686d5fe64f9fde6918a5ad72b5
MC_GK3😊 <zhenghaoyue134@gmail.com>
2026-01-18 16:39:25 +0800

    Update PvPBlockLootTableProvider.java

--- src/main/java/com/aiden/pvp/datagen/PvPBlockLootTableProvider.java
+++ src/main/java/com/aiden/pvp/datagen/PvPBlockLootTableProvider.java
@@ -18,27 +18,31 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
 public class PvPBlockLootTableProvider extends SimpleFabricLootTableProvider {
-    public PvPBlockLootTableProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
-        super(output, registryLookup, LootContextTypes.BLOCK);
-    }
+        public PvPBlockLootTableProvider(FabricDataOutput output,
+                        CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+                super(output, registryLookup, LootContextTypes.BLOCK);
+        }
 
-    public static final RegistryKey<LootTable> SPAWNER_BLOCK = RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.ofVanilla("blocks/spawner"));
-    public static final RegistryKey<LootTable> TRIAL_SPAWNER_BLOCK = RegistryKey.of(RegistryKeys.LOOT_TABLE, Identifier.ofVanilla("blocks/trial_spawner"));
+        public static final RegistryKey<LootTable> SPAWNER_BLOCK = RegistryKey.of(RegistryKeys.LOOT_TABLE,
+                        Identifier.ofVanilla("blocks/spawner"));
+        public static final RegistryKey<LootTable> TRIAL_SPAWNER_BLOCK = RegistryKey.of(RegistryKeys.LOOT_TABLE,
+                        Identifier.ofVanilla("blocks/trial_spawner"));
 
-
-    @Override
-    public void accept(BiConsumer<RegistryKey<LootTable>, LootTable.Builder> lootTableBiConsumer) {
-        lootTableBiConsumer.accept(SPAWNER_BLOCK, LootTable.builder()
-                .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
-                        .with(ItemEntry.builder(ModItems.BOSS_KEY)
-                                .apply(
-                                        SetCountLootFunction.builder(ConstantLootNumberProvider.create(1.0F))
-                                ))));
-        lootTableBiConsumer.accept(TRIAL_SPAWNER_BLOCK, LootTable.builder()
-                .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
-                        .with(ItemEntry.builder(ModItems.BOSS_KEY)
-                                .apply(
-                                        SetCountLootFunction.builder(ConstantLootNumberProvider.create(1.0F))
-                                ))));
-    }
+        @Override
+        public void accept(BiConsumer<RegistryKey<LootTable>, LootTable.Builder> lootTableBiConsumer) {
+                lootTableBiConsumer.accept(SPAWNER_BLOCK, LootTable.builder()
+                                .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
+                                                .with(ItemEntry.builder(ModItems.BOSS_KEY)
+                                                                .apply(
+                                                                                SetCountLootFunction.builder(
+                                                                                                ConstantLootNumberProvider
+                                                                                                                .create(1.0F))))));
+                lootTableBiConsumer.accept(TRIAL_SPAWNER_BLOCK, LootTable.builder()
+                                .pool(LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0F))
+                                                .with(ItemEntry.builder(ModItems.BOSS_KEY)
+                                                                .apply(
+                                                                                SetCountLootFunction.builder(
+                                                                                                ConstantLootNumberProvider
+                                                                                                                .create(1.0F))))));
+        }
 }
