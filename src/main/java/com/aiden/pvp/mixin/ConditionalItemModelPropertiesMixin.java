4a54a9c4849674f1b4c4d488d945cfd16583625f
Aiden8115 <3967083449@qq.com>
2026-03-07 22:23:04 +0800

    fixes

---
+++ src/main/java/com/aiden/pvp/mixin/ConditionalItemModelPropertiesMixin.java
@@ -0,0 +1,21 @@
+package com.aiden.pvp.mixin;
+
+import com.aiden.pvp.PvP;
+import com.aiden.pvp.client.item_properties_conditional.FishingRodCast;
+import net.minecraft.client.renderer.item.properties.conditional.ConditionalItemModelProperties;
+import net.minecraft.resources.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ConditionalItemModelProperties.class)
+public class ConditionalItemModelPropertiesMixin {
+    @Inject(
+            method = "bootstrap",
+            at = @At(value = "HEAD")
+    )
+    private static void bootstrap(CallbackInfo ci) {
+        ConditionalItemModelProperties.ID_MAPPER.put(Identifier.fromNamespaceAndPath(PvP.MOD_ID, "fishing_rod/cast"), FishingRodCast.MAP_CODEC);
+    }
+}
